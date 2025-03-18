ed5563bfe1d749fd9fc3f8d7133f53e2ae03825b
Abdullah Aleem <aaleem.bscs21seecs@seecs.edu.pk>
2025-03-18 23:48:04 +0500

    accesedEventAttributes

--- src/components/eventPopup.tsx
+++ src/components/eventPopup.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-function EventPopup(props: { close: () => void; info: object }) {
+function EventPopup(props: { close: () => void; info: any }) {
+  console.log("this is not okay ", props.info.event.title);
   return (
     <div className="absolute left-0 top-0 z-10 bg-gray-500  w-full h-full justify-center items-center flex bg-opacity-55 ">
       <div className="relative w-[800px] h-[600px] bg-white rounded-lg text-center  py-10 px-[60px]">
         <p className="text-2xl text-black text-center">Event Detail</p>
-        {props.info.title}
+        {props.info.event.title}
       </div>
     </div>
   );
